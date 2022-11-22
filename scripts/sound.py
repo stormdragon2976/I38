400722ca1d66cba0d5f9357bc4081bd093998759
Storm Dragon <stormdragon2976@gmail.com>
2022-11-22 17:53:53 -0500

    Nothing works yet, just holding my place befor actually getting started.

---
+++ scripts/sound.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+#This script allows for sounds in i3
+
+
+import os
+import i3ipc
+
+
+def play_sound(self, i3, event, sounds = sounds):
+    os.system(sounds[event]
+
+
+# sound command dictionary
+sounds = {'newWindow': 'play -n synth .25 sin 440:880 sin 480:920 remix - norm -3 pitch -500',
+    'modeKey': 'play -qV0 "|sox -np synth .07 sq 400" "|sox -np synth .5 sq 800" fade h 0 .5 .5 norm -20',
+    'modeDefaultKey': 'play -qV0 "|sox -np synth .07 sq 400" "|sox -np synth .5 sq 800" fade h 0 .5 .5 norm -20 reverse'}
+
+i3 = i3ipc.Connection()
+
+i3.on('window::new', play_sound("newWindow"))
+
+i3.main()
