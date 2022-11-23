5071d68864657ac928b3f39bc3da37770f3d94bc
Jeremiah Ticket <seashellpromises@gmail.com>
2022-11-23 14:10:22 -0700

    Commented out mode detection code for later use; will still play sound on mode change

--- scripts/sound.py
+++ scripts/sound.py
@@ -9,7 +9,11 @@ def on_new_window(self,i3):
     system('play -n synth .25 sin 440:880 sin 480:920 remix - norm -3 pitch -500')
 
 def on_mode(self,i3):
-    system('play -qV0 "|sox -np synth .07 sq 400" "|sox -np synth .5 sq 800" fade h 0 .5 .5 norm -20')
+#    mode = i3ipc.ModeEvent(['change'])
+#    if mode == 'resize':
+            system('play -qV0 "|sox -np synth .07 sq 400" "|sox -np synth .5 sq 800" fade h 0 .5 .5 norm -20')
+#    elif mode == 'default':
+#        system('play -qV0 "|sox -np synth .07 sq 400" "|sox -np synth .5 sq 800" fade h 0 .5 .5 norm -20 reverse')
 
 i3 = i3ipc.Connection()
 
