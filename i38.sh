aec05b9e776aae0add6c8614e1deabb56ead1f97
Storm Dragon <stormdragon2976@gmail.com>
2022-11-27 00:06:46 -0500

    Alt+play now shows current song info.

--- i38.sh
+++ i38.sh
@@ -266,6 +266,7 @@ bindsym XF86AudioLowerVolume exec --no-startup-id ${i3Path}/scripts/music_contro
 bindsym XF86AudioPrev exec --no-startup-id ${i3Path}/scripts/music_controler.sh prev
 bindsym XF86AudioMute exec --no-startup-id ${i3Path}/scripts/music_controler.sh pause
 bindsym XF86AudioPlay exec --no-startup-id ${i3Path}/scripts/music_controler.sh play
+bindsym \$mod+XF86AudioPlay exec --no-startup-id ${i3Path}/scripts/music_controler.sh info
 bindsym XF86AudioStop exec --no-startup-id ${i3Path}/scripts/music_controler.sh stop
 bindsym XF86AudioNext exec --no-startup-id ${i3Path}/scripts/music_controler.sh next
 
