7bffcda3b9293330729ecf114801de007a9fccb8
Jeremiah Ticket <seashellpromises@gmail.com>
2022-11-30 09:39:06 -0700

    Fixed formatting of mute-unmute.

--- scripts/mute-unmute.sh
+++ scripts/mute-unmute.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ $(pamixer --get-mute) = false ]; then
-spd-say -P important -Cw 'Muting!'
-pamixer -t
+    spd-say -P important -Cw 'Muting!'
+    pamixer -t
 else
-pamixer -t
-play -qnG synth 0.05 sin 440
-spd-say -P important -Cw 'Unmuted!'
+    pamixer -t
+    play -qnG synth 0.05 sin 440
+    spd-say -P important -Cw 'Unmuted!'
 fi
