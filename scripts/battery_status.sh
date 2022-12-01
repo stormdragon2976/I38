722a028c8f2a5c2b3c626151035b4cbf1bac5a87
Aaron Hewitt <aaron.graham.hewitt@gmail.com>
2022-12-01 19:23:52 +0000

    Actually applied the update properly.

--- scripts/battery_status.sh
+++ scripts/battery_status.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash
 
 #check for acpi
 if command -v acpi &> /dev/null; then
+    bat=$(acpi -b)
     spd-say -P important -Cw "$bat"
 else
     find /sys/class/power_supply -type l -exec bash -c '
